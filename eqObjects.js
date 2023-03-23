644f9af4c4befae2b661086df9c7b2356d1c5f9a
Gautam Khosla <gautamkhosla75@gmail.com>
2023-03-23 18:29:53 -0400

    Lotide Project initial commit

---
+++ eqObjects.js
@@ -0,0 +1,33 @@
+const eqArrays = require("./eqArrays");
+
+const eqObjects = function (object1, object2) {
+    if (Object.keys(object1).length !== Object.keys(object2).length) {
+        return false;
+    }
+    for (let key in object1) {
+        if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+            return eqArrays(object1[key], object2[key]);
+        } else if ((typeof (object1[key]) !== typeof ({}) && typeof (object2[key]) !== typeof ({})) && (object1[key] !== object2[key])) {
+            return false;
+        } else if ((typeof (object1[key]) === typeof ({}) || typeof (object1[key]) === typeof ({})) && eqObjects(object1[key], object2[key]) === false) {
+            return false;
+        } else if (typeof (object1[key]) === typeof ({}) || typeof (object1[key]) === typeof ({})) {
+            eqObjects(object1[key], object2[key]);
+        }
+    }
+    return true;
+};
+
+//same number of keys (check length)
+//same keys
+//same values
+//if the value is an array use eqArrays
+
+// let makeTree = (categories, parent) => {
+//   let node = {}
+//   categories
+//     .filter(c => c.parent === parent)
+//     .forEach(c => node[c.id] = makeTree(categories, c.id))
+//   return node
+// }
+module.exports = eqObjects;
---
+++ flatten.js
@@ -0,0 +1,23 @@
+const flatten = function (array) {
+    let newArray = [];
+    array.forEach(function (element) {
+        if (Array.isArray(element)) {
+            element.forEach(function (element2) {
+                newArray.push(element2);
+            });
+        } else {
+            newArray.push(element);
+        }
+    });
+    return newArray;
+};
+
+module.exports = flatten;
+
+/* if (Array.isArray(element)) {
+      array.splice(index, 1)
+      for (var x = element.length - 1; x >= 0; --x) {
+        console.log(element)
+        array.splice(index, 0, element[x])
+      }
+    }*/
