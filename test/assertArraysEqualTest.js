d659633c674454158b571d810f96fe9496706c7f
Gautam Khosla <gautamkhosla75@gmail.com>
2023-05-08 20:15:34 -0400

    Fixed error

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-const assertArraysEqual = require("../assertArrayEqual");
-=======
+
 const assertArraysEqual = require("../assertArraysEqual");
->>>>>>> updates
+
+
 
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
